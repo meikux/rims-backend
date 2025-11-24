f179e494764d0fa041d84391db04f6b7ba914ddb
Jomar Cotejo <jomarcotejo@gmail.com>
2025-11-24 22:40:50 +1100

    added auth route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/auth');
 
 dotenv.config(); // Load .env
 connectDB();     // Connect to MongoDB
@@ -9,6 +10,7 @@ connectDB();     // Connect to MongoDB
 const app = express();
 app.use(express.json());
 
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['manager', 'staff'], default: 'staff' }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+
+// Compare password
+userSchema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
